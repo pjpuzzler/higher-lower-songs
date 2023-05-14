f3e8692f6fcb9e2028da8c10c5575e4c58c1f13c
prj7465 <prj7465@rit.edu>
2023-05-14 10:20:28 -0400

    c

--- script.js
+++ script.js
@@ -1334,13 +1334,13 @@ function setVolume(newVolume) {
         fadeOut();
     } else if (currVolume === oldVolume) $elTrackPlayer[0].volume = volume;
 
-    if (volume / MAX_VOLUME > 2 / 3) {
+    if (linearVolume / MAX_VOLUME > 2 / 3) {
         elMuteBtn.innerHTML =
             '<svg id="mute_img" viewBox="0 0 16 16"><path d="M9.741.85a.75.75 0 01.375.65v13a.75.75 0 01-1.125.65l-6.925-4a3.642 3.642 0 01-1.33-4.967 3.639 3.639 0 011.33-1.332l6.925-4a.75.75 0 01.75 0zm-6.924 5.3a2.139 2.139 0 000 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 010 4.88z"></path><path d="M11.5 13.614a5.752 5.752 0 000-11.228v1.55a4.252 4.252 0 010 8.127v1.55z"></path></svg>';
-    } else if (volume / MAX_VOLUME > 1 / 3) {
+    } else if (linearVolume / MAX_VOLUME > 1 / 3) {
         elMuteBtn.innerHTML =
             '<svg id="mute_img" viewBox="0 0 16 16"><path d="M9.741.85a.75.75 0 01.375.65v13a.75.75 0 01-1.125.65l-6.925-4a3.642 3.642 0 01-1.33-4.967 3.639 3.639 0 011.33-1.332l6.925-4a.75.75 0 01.75 0zm-6.924 5.3a2.139 2.139 0 000 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 6.087a4.502 4.502 0 000-8.474v1.65a2.999 2.999 0 010 5.175v1.649z"></path></svg>';
-    } else if (volume > 0) {
+    } else if (linearVolume > 0) {
         elMuteBtn.innerHTML =
             '<svg id="mute_img" viewBox="0 0 16 16"><path d="M9.741.85a.75.75 0 01.375.65v13a.75.75 0 01-1.125.65l-6.925-4a3.642 3.642 0 01-1.33-4.967 3.639 3.639 0 011.33-1.332l6.925-4a.75.75 0 01.75 0zm-6.924 5.3a2.139 2.139 0 000 3.7l5.8 3.35V2.8l-5.8 3.35zm8.683 4.29V5.56a2.75 2.75 0 010 4.88z"></path></svg>';
     } else {
