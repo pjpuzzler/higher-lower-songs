e6f7d981297d9294723a6021698b2cbe829aac93
prj7465 <prj7465@rit.edu>
2024-11-16 19:07:12 -0500

    c

--- constants.js
+++ constants.js
@@ -54,7 +54,7 @@ const REDIRECT_URI = "https://pjpuzzler.github.io/higher-lower-songs/",
             // maxSearchResults: 500,
             difficulty: null,
             query: {
-                years: `${CURR_YEAR - 10}-${CURR_YEAR}`,
+                years: `${CURR_YEAR - 5}-${CURR_YEAR}`,
             },
             use: "search",
             uri: "",
